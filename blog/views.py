75ec5ef7b0c6315e17c0c453fa36f779ca4c5360
desmond <monkeydesmond@gmail.com>
2015-06-24 01:25:43 +0800

    debugging post

--- blog/views.py
+++ blog/views.py
@@ -11,27 +11,11 @@ def home_view(request):
     return render(request, "blog/home.html")
 
 def profile_view(request):
-    return render(request, "blog/profile.html")
+
 
 
 def post_view(request):
-    posts_list = Post.objects.all()
-    posts_snippets = list(map(lambda x: x.text[:250], posts_list))
-    posts_snippets = list(map(lambda x: x + " ...", posts_snippets))
-
-    paginator = Paginator(posts_list, 3)
-    page = request.GET.get('page')
-    '''
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        posts = paginator.page(paginator.num_pages)
-    '''
-    return render(request, "blog/posts.html", {"posts":posts_list})
+    return render(request, "blog/profile.html")
 
 
 
