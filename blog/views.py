5601a7bf5a3c5514dc5977e864ad8f13f6899b9e
desmond <monkeydesmond@gmail.com>
2015-06-24 02:06:53 +0800

    debugging post

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def profile_view(request):
 
 
 def post_view(request):
-    posts = Post.objects.all()
+    postsss = Post.objects.all()
     '''
     paginator = Paginator(posts_list, 3)
     page = request.GET.get('page')
@@ -28,7 +28,7 @@ def post_view(request):
         # If page is out of range (e.g. 9999), deliver last page of results.
         posts = paginator.page(paginator.num_pages)
     '''
-    return render(request, "blog/posts.html", {"posts":posts})
+    return render(request, "blog/posts.html", {"postsss":postsss})
 
 
 
