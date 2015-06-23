5f9cd04c7a25b653fcf48a94b7d6fc54a0bea800
desmond <monkeydesmond@gmail.com>
2015-06-24 01:08:37 +0800

    configure Static_root

--- desmondblog/settings.py
+++ desmondblog/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = ''
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
