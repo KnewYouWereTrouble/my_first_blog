6d4c2aef234f9bcb17df2636f3a887fdf9a342a7
desmond <monkeydesmond@gmail.com>
2015-06-24 00:42:37 +0800

    additional files and changes for heroku

--- desmondblog/settings.py
+++ desmondblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fs^=z1!hc%!x028yxu@j)gar))vg_8wltawrox0e!^0&nl02un'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -101,3 +101,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+DEBUG = False
+try:
+    from .local_settings import *
+except ImportError:
+    pass
