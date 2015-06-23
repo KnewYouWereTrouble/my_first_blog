ddd5f2887cfe5d14f759c087fbddb2bfdd3a78e7
desmond <monkeydesmond@gmail.com>
2015-06-24 01:50:47 +0800

    debugging post

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,7 @@ def post_view(request):
         # If page is out of range (e.g. 9999), deliver last page of results.
         posts = paginator.page(paginator.num_pages)
     '''
-    return render(request, "blog/posts.html", {"posts":posts_list})
+    return render(request, "blog/posts.html", {"posts_list":posts_list})
 
 
 
